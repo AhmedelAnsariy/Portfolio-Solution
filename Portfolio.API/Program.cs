c4ccacdb30ca280b7ce874beaa2f0759bd29b29a
AhmedelAnsariy <ahmed.elansariy@gmail.com>
2024-08-18 06:18:03 +0300

    Add Update Database

--- Portfolio.API/Program.cs
+++ Portfolio.API/Program.cs
@@ -5,7 +5,7 @@ namespace Portfolio.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var webApplicationbuilder = WebApplication.CreateBuilder(args);
 
@@ -28,6 +28,22 @@ namespace Portfolio.API
 
 
             var app = webApplicationbuilder.Build();
+
+
+            using var scope = app.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var _context = services.GetRequiredService<DataDbContext>();
+
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+            try
+            {
+                await _context.Database.MigrateAsync();
+            }catch (Exception ex)
+            {
+                var loogger = loggerFactory.CreateLogger<Program>();
+                loogger.LogError(ex, "Error in Update Database");
+            }
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
