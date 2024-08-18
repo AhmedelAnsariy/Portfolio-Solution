934b0dc9b1d329d2840ab9e988afe862dadff63c
AhmedelAnsariy <ahmed.elansariy@gmail.com>
2024-08-18 04:09:57 +0300

    Add project files.

---
+++ Portfolio.Core/Models/ClientReview.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Core.Models
+{
+    public class ClientReview : BaseClass
+    {
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public string Description { get; set; }
+        public string PictureUrl { get; set; }
+
+
+    }
+}
---
+++ Portfolio.Core/Models/Design.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Core.Models
+{
+    public class Design : BaseClass
+    {
+        public string Name { get; set; }
+
+        public string PictureUrl { get; set; }
+
+
+
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+
+
+    }
+}
